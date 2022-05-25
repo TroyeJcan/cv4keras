0fcec22542393a5daf95a3676edac2e381fea60a
陈帆 <chenfan1@jianke.com>
2022-05-25 11:52:36 +0800

    first commit

---
+++ cv4keras/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2022.5.25 11:45
+Location jianke
+
+@author: Troye Jcan
+"""
+__version__ = "0.0.1"
---
+++ example/README.md
@@ -0,0 +1,5 @@
+# 例子合集
+提示：GitHub上的Example是用tensorflow2.8.0做的，如果报错可以尝试升级tensorflow
+## 简介
+* 暂无
+
